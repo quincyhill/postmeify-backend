b9f92efdbef2247631dff6691179bd0f99d5263d
quincyhill <therealquincyhill@gmail.com>
2022-02-20 01:34:17 -0600

    Switched from express to java spring

--- src/app.ts
+++
@@ -1,9 +0,0 @@
-import express, { Application, Request, Response, NextFunction } from 'express'
-
-const app: Application = express()
-
-app.get('/', (req: Request, res: Response, next: NextFunction) => {
-  res.send('Hello World!')
-})
-
-app.listen(5000, () => console.log('server running on port 5000...'))
