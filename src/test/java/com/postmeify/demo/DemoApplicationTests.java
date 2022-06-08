403baddef7f53ce59d5ef21751456381de8fa735
quincyhill <therealquincyhill@gmail.com>
2022-06-08 14:55:00 -0500

    Updated the backend

--- src/test/java/com/postmeify/demo/DemoApplicationTests.java
+++ src/test/java/com/postmeify/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.realtorsunited.backend;
+package com.postmeify.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
